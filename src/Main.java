1435936206bc3d686126c33bc5873f96cc182a47
tejveer <tejveerseoli@gmail.com>
2025-02-15 12:00:42 +0530

    cc
    cc

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,20 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        for(int i=0;i<5;i++){
-            for (int j=0;j<i;j++){
+//        for(int i=0;i<5;i++){
+//            for (int j=0;j<i;j++){
+//
+//            System.out.print("*");
+//            }
+//            System.out.println("");
+//        }
 
-            System.out.print("*");
+
+        for(int i=0;i<4;i++){
+            for(int j = 0;j<i;j++){
+                
             }
-            System.out.println("");
+            System.out.println("*");
         }
 
 
